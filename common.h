59dfedfe217bef58a78d87403a4c6decd359b9f6
Zakhar Telyatnikov <tz@za-ek.ru>
2018-09-10 23:49:01 +0300

    Create db command

--- common.h
+++ common.h
@@ -1,1 +1,23 @@
+/**
+ * @brief z_trim        Remove leading and trailing spaces from string
+ * @param str
+ * @return
+ */
 char *z_trim(char *str);
+/**
+ * @brief z_err         Shows error
+ * @param err
+ */
+void z_err(char*err);
+/**
+ * @brief z_mkdir       Recursively create directory
+ * @param path
+ * @return
+ */
+int z_mkdir(char*path);
+/**
+ * @brief z_dirExist    Check if directory exists (return 1 if exist)
+ * @param path
+ * @return
+ */
+int z_dirExist(char*path);
--- main.c
+++ main.c
@@ -5,23 +5,31 @@
 #include <string.h>
 #include <errno.h>
 #include <unistd.h>
-
+#include <string.h>
 #include <netdb.h>
 #include <netinet/in.h>
 #include <stdio.h>
 
+#include "common.h"
+#include "db2.h"
+
 #define PORT_NO 2450
+#define STORAGE_PATH "/var/lib/intersectdb/storage/"
 
 void startSocket();
 void doprocessing(int sock);
+int checkStoragePath(char *path);
 
 int main()
 {
-    startSocket();
+    printf("Hi!\n");
+    if(checkStoragePath(STORAGE_PATH) == 1) {
+        startSocket(PORT_NO);
+    }
     return 0;
 }
 
-void startSocket()
+void startSocket(int port)
 {
     int sockfd, newsockfd;
     socklen_t clilen;
@@ -43,7 +51,7 @@ void startSocket()
 
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = INADDR_ANY;
-    serv_addr.sin_port = htons(PORT_NO);
+    serv_addr.sin_port = htons(port);
 
     if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
         perror("ERROR on binding");
@@ -91,6 +99,7 @@ void doprocessing (int sock)
 {
     int n;
     char buffer[256];
+    char *cmd, *string, *token;
 
     bzero(buffer,256);
     n = read(sock,buffer,255);
@@ -102,6 +111,41 @@ void doprocessing (int sock)
 
     printf("Received the message: %s\n",buffer);
 
+
+    string = strdup(buffer);
+    string = z_trim(string);
+
+    if((token = strsep(&string, " ")) != NULL) {
+        printf("Found <%s>\n", token);
+
+        cmd = token;
+
+        write(sock, "Command found:", 14);
+        write(sock, cmd, strlen(cmd));
+        write(sock, "\n", 1);
+
+        if(strcmp("CREATE2", cmd) == 0) {
+            char*db_name;
+            db_name = strsep(&string, " ");
+
+            int db_size;
+            db_size = atoi(strsep(&string, " "));
+
+            write(sock, "Will create a database: <", 25);
+            write(sock, db_name, strlen(db_name));
+            write(sock, ":", 1);
+
+            char tmp[12]={0x0};
+            sprintf(tmp,"%i", db_size);
+            write(sock, tmp, strlen(tmp));
+
+            write(sock, ">\n", 2);
+
+            printf("Create database <%s> with <%i> elements\n", db_name, db_size);
+            intersect2_createDb(STORAGE_PATH, db_name, db_size);
+        }
+    }
+
     n = write(sock,"I got your message\n",19);
 
     if (n < 0) {
@@ -110,3 +154,23 @@ void doprocessing (int sock)
     }
 }
 
+/**
+ * @brief checkStoragePath
+ * @return int for correct
+ */
+int checkStoragePath(char*path)
+{
+    int result = z_dirExist(path);
+    if (result == 1) {
+        return 1;
+    } else if (result == 2) {
+        char*err = "";
+        // So poor program :(
+        sprintf(err, "Please create a directory <%s>", path);
+        z_err(err);
+    } else {
+        z_err("Couldn't check storage path");
+    }
+
+    return 2;
+}
