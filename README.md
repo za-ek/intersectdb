f5189158d5eb4b04d856ad6dafa40e1a2da53b49
Zakhar Telyatnikov <tz@za-ek.ru>
2018-09-11 13:01:26 +0300

    Increment/Fetch functions

--- README.md
+++ README.md
@@ -6,3 +6,11 @@ Start socket on 2450
 Create a database file
 `CREATE2 db_name db_size`
 `CREATE2 cities 1230`
+
+Increment value
+`INC db_name N M`
+`INC cities 2 43`
+
+Fetch value
+`FETCH db_name N M`
+`FETCH cities 32 2`
--- db2.h
+++ db2.h
@@ -1,21 +1,26 @@
 /**
+ * @brief storage_path
+ */
+extern char storage_path[255];
+
+/**
  * @brief intersect2_createDb
  * @param dir       Path to storage directory
  * @param db_name   Database name
  * @param dicSize   Element count of the dictionary
  * @return
  */
-int intersect2_createDb(char*dir, char*db_name, int16_t dicSize);
+int intersect2_createDb(char*db_name, int16_t dicSize);
 /**
  * @brief intersect2_inc    Increment intersection of two IDs
  * @param e1
  * @param e2
  */
-void intersect2_inc(int16_t e1, int16_t e2);
+int intersect2_inc(char*db_name, int e1, int e2);
 /**
  * @brief intersect2_get    Return value of intersection of two elements
  * @param e1
  * @param e2
  * @return
  */
-int intersect2_get(int16_t e1, int16_t e2);
+int intersect2_get(char*db_name, int e1, int e2);
